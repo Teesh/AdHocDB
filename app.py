390ed2fd1921586859392e34cb18315b7ce7925e
Sneha K <srkrish2@illinois.edu>
2018-07-07 21:45:35 -0500

    sql parsing

--- app.py
+++ app.py
@@ -1,32 +1,100 @@
-import click
+import argparse
+import sqlparse
+from sqlparse.sql import *
+from sqlparse.tokens import *
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def main(count, name):
-    i = input("Choose an option (e-exit, f-add files, q-query)");
-    while i != "e":
-        if i == "f":
-            open_files()
-        elif i == "q":
-            query()
-        else: #nothing matched
-            print "Nothing matched\n"
-    #for x in range(count):
-    #    click.echo('Hello %s!' % name)
+def main():
+  parser = argparse.ArgumentParser(description='Preprocessing files and running queries.')
+  parser.add_argument('--preprocess', metavar = 'Files', nargs='+', type=str,
+                      help='Preprocess files where files are given in a space seperated list. Ex. File1 File2 ...')
+  parser.add_argument('--query', type=str,  nargs='+',
+                      help='Run a query with the syntax SELECT <columns> FROM <tables> WHERE <condition>')
 
-def open_files():
-    i = input("Type in a comma seperated list of files: ")
-    return
+  args = parser.parse_args()
+  query = args.query
+  preprocess = args.preprocess
+  if preprocess is None:
+    #go to query function since no preprocessing needed
+    query = ' '.join(query)
+    print("here")
+    parsing(query)
+    
+  if query is None:
+    #go to preprocessing function
+    print("here2")
 
-def query():
-    print """The query must be as follows:
-             SELECT attr1, attr2, .... attrn
-             FROM R1, R2, ... Rn
-             WERE C1 AND ... AND Ck /n"""
 
-    return
+def parsing(query):
+  select = -1
+  from_ind = -1
+  where_ind = -1
 
-if __name__ == '__main__':
+  parsed = sqlparse.parse(query)[0]
+  print(parsed.tokens)
+
+  for item in parsed.tokens:
+    print(item.ttype)
+    if item.ttype is DML and item.value.upper() == 'SELECT':
+      select = parsed.tokens.index(item)
+    elif item.ttype is Keyword and item.value.upper() == 'FROM':
+      from_ind = parsed.tokens.index(item)
+    elif item.ttype is None and isinstance(item, Where):
+      where_ind = parsed.tokens.index(item)
+
+  print(select, from_ind, where_ind)
+
+  select_columns = get_columns_or_tables(parsed, select, from_ind)
+  from_tables = get_columns_or_tables(parsed, from_ind, where_ind)
+  get_tables(from_tables)
+  where_condition = get_conditions(parsed, where_ind)
+
+  #make sure that all the attributes in the select and where
+  #match with the attributes of the tables
+  verify_attributes()
+
+  #figure out how exactly to do the computations
+  query_plan()
+
+
+
+def get_tables(table_list):
+  for table in table_list:
+    #use pandas here to upload the tables into memory
+    pass
+
+
+def verify_attributes():
+  pass
+
+
+def query_plan():
+  pass
+
+
+def get_columns_or_tables(parsed, start, stop):
+  token_stream = parsed.tokens[start:stop]
+  print(token_stream)
+  case = -1
+  for item in token_stream:
+    if isinstance(item, IdentifierList) or isinstance(item, Identifier): 
+      print(item.value.split(','))
+      return item.value.split(',')
+
+
+def get_conditions(parsed, start):
+  token_stream = parsed.tokens[start:]
+  parsed_where = next(token for token in token_stream if isinstance(token, Where))[1:]
+  # print(parsed_where)
+  # print(parsed_where[1].value)
+  conditions = []
+  for token in parsed_where:
+    if token.ttype is Keyword:
+      conditions.append(token.value)
+    if token.ttype is None:
+      conditions.append(token.value)
+  print(conditions)
+  return conditions
+
+
+if __name__ == "__main__":
     main()
