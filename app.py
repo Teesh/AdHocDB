6158a868ba7ab49904bbc676220b3cc25de0a911
Sneha K <srkrish2@illinois.edu>
2018-07-15 17:19:53 -0500

    done with one table queries

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import sqlparse
 import pandas
 from sqlparse.sql import *
 from sqlparse.tokens import *
+import re
+import time
 
-#conds = ["movies.movie_title == 'King Kong'","movies.actor_1_facebook_likes < 20000","actor_2_facebook_likes > actor_1_facebook_likes","movies.title_year < 2010"]
+conds = ["movies.movie_title == 'King Kong'","movies.actor_1_facebook_likes < 20000","actor_2_facebook_likes > actor_1_facebook_likes","movies.title_year < 2010"]
 
-#movies = pandas.read_csv('../Datasets/Movies/movies.csv')
+#movies = pandas.read_csv('./movies.csv')
 
 #print(movies[eval(conds[1]) & eval(conds[3])])
+
+path = "../Datasets/Movies/"
 
 def main():
   parser = argparse.ArgumentParser(description='Preprocessing files and running queries.')
@@ -23,28 +27,29 @@ def main():
   if preprocess is None:
     #go to query function since no preprocessing needed
     query = ' '.join(query)
-    print("here")
+    # print("here")
     parsing(query)
-    
+
   if query is None:
     #go to preprocessing function
     preprocessing()
-    print("here2")
+    # print("here2")
 
 
 def preprocessing():
-
+  pass
 
 def parsing(query):
+  start = time.time()
   select = -1
   from_ind = -1
   where_ind = -1
 
   parsed = sqlparse.parse(query)[0]
-  print(parsed.tokens)
+  # print(parsed.tokens)
 
   for item in parsed.tokens:
-    print(item.ttype)
+    # print(item.ttype)
     if item.ttype is DML and item.value.upper() == 'SELECT':
       select = parsed.tokens.index(item)
     elif item.ttype is Keyword and item.value.upper() == 'FROM':
@@ -63,43 +68,78 @@ def parsing(query):
   	from_tables = get_columns_or_tables(parsed, from_ind, len(parsed.tokens))
 
   #get columns that are listed in where clause
-  get_colums_from_whereclause()
+  #get_colums_from_whereclause()
 
-  #get the relevant columns from each table as required by the select and where functions
-  get_tables(from_tables, select_columns, where_condition)
   #make sure that all the attributes in the select and where
   #match with the attributes of the tables
   #verify_attributes()
 
   #figure out how exactly to do the computations
-  query_plan()
+  result = query_plan(from_tables, where_condition)
+
+  print(projection(result, select_columns))
+
+  end = time.time()
+  print("Time:", end-start)
   #
 
   #More to do...not sure what yet
-  #print(movies[eval(where_condition[0])]); 
+  print("actual result")
+  # print(movies[eval(conds[1]) & eval(conds[3])])
 
 
-def get_tables(table_list):
+def projection(table, columns):
+    '''
+    get the final result table and a list of columns
+    '''
+    return table[columns]
+
+
+def query_plan(table_list, where_condition):
+  #get tables that are needed
   for table in table_list:
     #use pandas here to upload the tables into memory
+    globals()[table] = eval('pandas.read_csv("' +path+ table + '.csv")')
+    # eval(table + "=pandas.read_csv('./" + table + ".csv')")
+
+  if len(table_list) == 1:
+      cond_str = table_list[0] + create_cond_str(where_condition)
+      return eval(cond_str)
+
+
+def create_cond_str(where_condition):
+    print(where_condition)
+    cond_str = "["
+    for w in where_condition:
+        if w.upper() == "AND":
+            cond_str = cond_str + ' & '
+        elif w.upper() == 'OR':
+            cond_str = cond_str + ' | '
+        elif w.upper() == 'NOT':
+            cond_str = cond_str + ' !'
+        else:
+            cond_str = cond_str + 'eval("' + w + '")'
+    cond_str = cond_str + ']'
+    print(cond_str)
+    return cond_str
+
+def parse_where(condition):
+    '''
+    count how many dots to determine
+    return t (list of strings): the table names in that query
+           c (list of columns): the columns used in that query if any
+    '''
+
     pass
-
-
-def verify_attributes():
-  pass
-
-
-def query_plan():
-  pass
 
 
 def get_columns_or_tables(parsed, start, stop):
   '''
-  Returns the names of the columns after the SELECT keyword or 
+  Returns the names of the columns after the SELECT keyword or
   the names of the tables after the FROM keyword
 
-  Input: 
-    parsed List: Parsed query tokens 
+  Input:
+    parsed List: Parsed query tokens
     start int: starting point of SELECT or FROM
     stop int: ending point of SELECT or From statement
 
@@ -109,15 +149,15 @@ def get_columns_or_tables(parsed, start, stop):
   token_stream = parsed.tokens[start:stop]
   print(token_stream)
   for item in token_stream:
-    if isinstance(item, IdentifierList) or isinstance(item, Identifier): 
-      print(item.value.split(','))
+    if isinstance(item, IdentifierList) or isinstance(item, Identifier):
+    #   print(item.value.split(','))
       return item.value.split(',')
 
 
 def get_conditions(parsed, start):
   '''
   Gets the conditions from the where clause and returns them in a list
-  The conditions are kept in the same order as written and the operators 
+  The conditions are kept in the same order as written and the operators
   are also there in the same order
 
   Returns: List of strings
@@ -132,7 +172,7 @@ def get_conditions(parsed, start):
       conditions.append(token.value)
     if token.ttype is None:
       conditions.append(token.value)
-  print(conditions)
+  # print(conditions)
   return conditions
 
 
