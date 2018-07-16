165e0e4de097ed94cd6fba47c5b134a2390a37e3
Sneha K <srkrish2@illinois.edu>
2018-07-16 11:06:51 -0500

    one table finalized

--- app.py
+++ app.py
@@ -12,67 +12,65 @@ conds = ["movies.movie_title == 'King Kong'","movies.actor_1_facebook_likes < 20
 
 #print(movies[eval(conds[1]) & eval(conds[3])])
 
-path = "../Datasets/Yelp/"
+path = "../Datasets/Movies/"
 
 def main():
-  parser = argparse.ArgumentParser(description='Preprocessing files and running queries.')
-  parser.add_argument('--preprocess', metavar = 'Files', nargs='+', type=str,
+    parser = argparse.ArgumentParser(description='Preprocessing files and running queries.')
+    parser.add_argument('--preprocess', metavar = 'Files', nargs='+', type=str,
                       help='Preprocess files where files are given in a space seperated list. Ex. File1 File2 ...')
-  parser.add_argument('--query', type=str,  nargs='+',
+    parser.add_argument('--query', type=str,  nargs='+',
                       help='Run a query with the syntax "SELECT <columns> FROM <tables> WHERE <condition>". Note: The quotations around the SELECT statement are very important.')
 
-  args = parser.parse_args()
-  query = args.query
-  preprocess = args.preprocess
-  if preprocess is None:
-    #go to query function since no preprocessing needed
-    query = ' '.join(query)
-    # print("here")
-    parsing(query)
+    args = parser.parse_args()
+    query = args.query
+    preprocess = args.preprocess
+    if preprocess is None:
+        #go to query function since no preprocessing needed
+        query = ' '.join(query)
+        parsing(query)
 
-  if query is None:
-    #go to preprocessing function
-    preprocessing()
-    # print("here2")
+    if query is None:
+        #go to preprocessing function
+        preprocessing()
 
 
 def preprocessing():
-  pass
+    pass
 
 def parsing(query):
-  start = time.time()
-  select = -1
-  from_ind = -1
-  where_ind = -1
+    start = time.time()
+    select = -1
+    from_ind = -1
+    where_ind = -1
+    parsed = sqlparse.parse(query)[0]
+    # print(parsed.tokens)
 
-  parsed = sqlparse.parse(query)[0]
-  # print(parsed.tokens)
-
-  for item in parsed.tokens:
+    for item in parsed.tokens:
     # print(item.ttype)
-    if item.ttype is DML and item.value.upper() == 'SELECT':
-      select = parsed.tokens.index(item)
-    elif item.ttype is Keyword and item.value.upper() == 'FROM':
-      from_ind = parsed.tokens.index(item)
-    elif item.ttype is None and isinstance(item, Where):
-      where_ind = parsed.tokens.index(item)
+        if item.ttype is DML and item.value.upper() == 'SELECT':
+            select = parsed.tokens.index(item)
+        elif item.ttype is Keyword and item.value.upper() == 'FROM':
+            from_ind = parsed.tokens.index(item)
+        elif item.ttype is None and isinstance(item, Where):
+            where_ind = parsed.tokens.index(item)
 
-  print(select, from_ind, where_ind, len(parsed.tokens))
+    print(select, from_ind, where_ind, len(parsed.tokens))
 
-  if (where_ind > 0):
-  	select_columns = get_columns_or_tables(parsed, select, from_ind)
-  	from_tables = get_columns_or_tables(parsed, from_ind, where_ind)
-  	where_condition = get_conditions(parsed, where_ind)
-  else:
-  	select_columns = get_columns_or_tables(parsed, select, from_ind)
-  	from_tables = get_columns_or_tables(parsed, from_ind, len(parsed.tokens))
+    if (where_ind > 0):
+      	select_columns = get_select_names(parsed, select, from_ind)
+      	from_tables = get_table_names(parsed, from_ind, where_ind)
+      	where_condition = get_conditions(parsed, where_ind)
+    else:
+      	select_columns = get_select_names(parsed, select, from_ind)
+      	from_tables = get_table_names(parsed, from_ind, len(parsed.tokens))
 
-  #figure out how exactly to do the computations
-  result = query_plan(from_tables, where_condition)
-  print(projection(result, select_columns))
+    #figure out how exactly to do the computations
+    result = query_plan(from_tables, where_condition)
+    print(select_columns)
+    print(projection(result, select_columns))
 
-  end = time.time()
-  print("Time:", end-start)
+    end = time.time()
+    print("Time:", end-start)
 
 
 def projection(table, columns):
@@ -83,15 +81,27 @@ def projection(table, columns):
 
 
 def query_plan(table_list, where_condition):
-  #get tables that are needed
-  for table in table_list:
-    #use pandas here to upload the tables into memory
-    globals()[table] = eval('pandas.read_csv("' +path+ table + '.csv")')
+    #get tables that are needed
+    for table in table_list:
+        #use pandas here to upload the tables into memory
+        print(table)
+        if(len(table) == 2):
+            globals()[table[1]] = eval('pandas.read_csv("' +path+ table[0] + '.csv")')
+        else:
+            print(table[0])
+            globals()[table[0]] = eval('pandas.read_csv("' +path+ table[0] + '.csv")')
 
 
-  if len(table_list) == 1:
-      cond_str = table_list[0] + create_cond_str(where_condition)
-      return eval(cond_str)
+    if len(table_list) == 1:
+        if(len(table_list[0]) == 1):
+            cond_str = table_list[0][0] + create_cond_str(where_condition)
+        else:
+            cond_str = table_list[0][1] + create_cond_str(where_condition)
+        return eval(cond_str)
+
+    else:
+        return create_bst(where_condition)
+
 
 def rename_columns(table):
     columns = list(eval(table))
@@ -125,26 +135,35 @@ def parse_where(condition):
 
     pass
 
+def get_select_names(parsed,start,stop):
+    print("in select names")
+    token_stream = parsed.tokens[start:stop]
+    print(token_stream)
+    for item in token_stream:
+        if isinstance(item, IdentifierList) or isinstance(item, Identifier):
+            x = item.value.split(',')
+            y = [t.strip() for t in x]
+            return y
 
-def get_columns_or_tables(parsed, start, stop):
-  '''
-  Returns the names of the columns after the SELECT keyword or
-  the names of the tables after the FROM keyword
+def get_table_names(parsed, start, stop):
+    '''
+    Returns the names of the columns after the SELECT keyword or
+    the names of the tables after the FROM keyword
 
-  Input:
-    parsed List: Parsed query tokens
-    start int: starting point of SELECT or FROM
-    stop int: ending point of SELECT or From statement
+    Input:
+        parsed List: Parsed query tokens
+        start int: starting point of SELECT or FROM
+        stop int: ending point of SELECT or From statement
 
-  Returns: List of strings
-
-  '''
-  token_stream = parsed.tokens[start:stop]
-  print(token_stream)
-  for item in token_stream:
-    if isinstance(item, IdentifierList) or isinstance(item, Identifier):
-    #   print(item.value.split(','))
-      return item.value.split(',')
+    Returns: [["table1", "t1"],["table2", "t2"]...]
+    '''
+    token_stream = parsed.tokens[start:stop]
+    print(token_stream)
+    for item in token_stream:
+        if isinstance(item, IdentifierList) or isinstance(item, Identifier):
+            tables = item.value.split(',')
+            y = [t.strip().split(' ') for t in tables]
+            return y
 
 
 def get_conditions(parsed, start):
