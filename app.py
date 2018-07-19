ac7b64845da9e94b9458ee28217272a88cd262c5
Sneha K <srkrish2@illinois.edu>
2018-07-19 10:28:54 -0500

    starting to debug

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def parsing(query):
       	from_tables = get_table_names(parsed, from_ind, len(parsed.tokens))
 
     #figure out how exactly to do the computations
-    result = query_plan(from_tables, where_condition)
+    result = query_plan(from_tables, where_condition, select_columns)
     print(select_columns)
     print(projection(result, select_columns))
 
@@ -80,16 +80,16 @@ def projection(table, columns):
     return table[columns]
 
 
-def query_plan(table_list, where_condition):
+def query_plan(table_list, where_condition, select_columns):
     #get tables that are needed
     for table in table_list:
         #use pandas here to upload the tables into memory
         print(table)
         if(len(table) == 2):
-            globals()[table[1]] = eval('pandas.read_csv("' +path+ table[0] + '.csv")')
+            globals()[table[1]] = eval('pandas.read_csv("' +path+ table[0] + '.csv", index_col=False)')
         else:
             print(table[0])
-            globals()[table[0]] = eval('pandas.read_csv("' +path+ table[0] + '.csv")')
+            globals()[table[0]] = eval('pandas.read_csv("' +path+ table[0] + '.csv", index_col=False)')
 
 
     if len(table_list) == 1:
@@ -100,10 +100,10 @@ def query_plan(table_list, where_condition):
         return eval(cond_str)
 
     else:
-        return eval_or(where_condition)
+        return eval_or(where_condition, select_columns)
 
 
-def eval_or(conditions):
+def eval_or(conditions, columns):
     cond_lower = [c.lower() for c in conditions]
     try:
         a = cond_lower.index('or')
@@ -111,9 +111,15 @@ def eval_or(conditions):
         right = conditions[a+1:]
         left_eval = eval_and(left)
         right_eval = eval_or(right)
-        return combine_or(left, right)
+        return combine_or(left, right, columns)
     except ValueError:
         return eval_and(conditions)
+
+
+def combine_or(table1, table2, columns):
+    result = table1.append(table2)
+    return result[columns]
+
 
 def eval_and(conditions):
     cond_lower = [c.lower() for c in conditions]
@@ -121,36 +127,76 @@ def eval_and(conditions):
         a = cond_lower.index('and')
         left = conditions[0:a]
         right = conditions[a+1:]
-        left_eval = eval_not(left)
-        right_eval = eval_and(right)
-        return combine_and(left, right)
+        #continue down the right subtree, and get a resulting table
+        right_table = eval_and(right)
+        #if there is a not, then negate the condition
+        left_cond = eval_not(left)
+        #compbine table from right with left cond
+        return combine_and(left_cond, right_table)
     except ValueError:
-        return eval_not(conditions)
+        #last condition, negate and then evaluate
+        cond = eval_not(conditions)
+        return eval_cond(cond)
 
 
-def eval_not(conditions):
-    cond_lower = [c.lower() for c in conditions]
+def eval_not(condition):
+    cond_lower = [c.lower() for c in condition]
     try:
         a = cond_lower.index('not')
-        neg = negate(conditions[a+1:])
-        return eval_cond(neg)
+        neg = negate(condition[a+1:])
+        return neg
     except ValueError:
-        return eval_cond(conditions)
+        return condition
 
 
-def eval_cond(conditions):
+def combine_and(left_cond, right_result):
+    left,binop,right = comparision_parse(left_cond)
+    left_left, arithm_op, left_right, binop, right = arithm_parse(left,binop,right)
+    ntable, left_table, right_table, left_col, right_col = check_num_table(left_left, right)
+    #left table should have been part of computations in right subtree
+    if arithm_op != None:
+        new_col = eval(right_result+"["+left_col"]"+arithm_op+left_left)
+        right_result.update(eval("pandas.DataFrame({'"left_col"': new_col})"))
+    #join using tmp and col with table from right subtree and right col
+    if ntable == 1 and left_col in right_result.columns:
+        #simple filter because column already in right_table
+        return(eval("right_result["+left_col+binop+right"]"))
+    else if ntable == 1 and left_col not in right_result.columns:
+        #join tables then filter
+        out = eval('right_result.merge('+left_table+')')
+        result = eval("out["left_col+binop+right"]")
+        return result
+    else:
+        #join and filter
+        out = eval('right_result.merge('+right_table+')')
+        result = eval("out["left_col+binop+right"]")
+        return result
+
+
+def eval_cond(condition):
     result = []
-    left,op,right = comparision_parse(conditions)
-    processed = arithm_parse(left,op,right)
-    result.append(processed)
-    return result
+    left,binop,right = comparision_parse(conditions)
+    left_left, arithm_op, left_right, binop, right = arithm_parse(left,binop,right)
+    ntable, left_table, right_table, left_col, right_col = check_num_table(left_left, right)
+    if ntable == 1:
+        #we can evaluate the condition here itself and return
+        cond_str = create_cond_str(condition)
+        return eval(cond_str)
+    else:
+        tmp = eval(left_table)
+        #eval arithm operator and replace left table with tmp
+        if arithm_op != None:
+            new_col = eval(left_table+"["+left_col"]"+arithm_op+left_left)
+            tmp.update(eval("pandas.DataFrame({'"left_col"': new_col})"))
+        #join
+        if binop == '=':
+            out = eval('tmp.merge('+right_table+', left_on="'+left_col+'", right_on="'+right_col+'")')
+            return out
+        else:
+            out = eval('tmp.merge('+right_table+')')
+            return eval("out["left_col+binop+right"]")
+        pass
 
-
-def combine_and(conditions):
-    pass
-
-def combine_or(conditions):
-    pass
 
 def negate(conditions):
     left,op,right = comparision_parse(conditions)
@@ -195,14 +241,6 @@ def create_cond_str(where_condition):
     print(cond_str)
     return cond_str
 
-def parse_where(condition):
-    '''
-    count how many dots to determine
-    return t (list of strings): the table names in that query
-           c (list of columns): the columns used in that query if any
-    '''
-
-    pass
 
 def get_select_names(parsed,start,stop):
     print("in select names")
@@ -216,8 +254,7 @@ def get_select_names(parsed,start,stop):
 
 def get_table_names(parsed, start, stop):
     '''
-    Returns the names of the columns after the SELECT keyword or
-    the names of the tables after the FROM keyword
+    Returns the names of the columns after the FROM keyword
 
     Input:
         parsed List: Parsed query tokens
@@ -256,75 +293,103 @@ def get_conditions(parsed, start):
   # print(conditions)
   return conditions
 
-def condition_check(conditions):
-  count = 0
-  result = []
-  for item in conditions:
-    if item.upper() != "AND" and item.upper() != "OR" and item.upper() != "NOT":
-        left,op,right = comparision_parse(item)
-        processed = arithm_parse(left,op,right)
-        result.append(processed)
-        count = count +1
-    else:
-        result.append(item)
-  result.insert(0,count)
-  return result
+# def condition_check(conditions):
+#   count = 0
+#   result = []
+#   for item in conditions:
+#     if item.upper() != "AND" and item.upper() != "OR" and item.upper() != "NOT":
+#         left,op,right = comparision_parse(item)
+#         processed = arithm_parse(left,op,right)
+#         result.append(processed)
+#         count = count +1
+#     else:
+#         result.append(item)
+#   result.insert(0,count)
+#   return result
 
-def arithm_parse(left,op,right):
+def arithm_parse_eval(left,op,right):
+    '''
+        Assuming that we only have one arithm op on left side
+    '''
+    # op_side = None
+    left_left = left
+    left_right = None
+    arithm_op = None
+    binary_op = op
+    # right_left = right
+    # right_right = None
     for char in left:
         if find_char_pos(left, '+') != -1 :
-            return ("left",left[0:find_char_pos(left, '+')],"+",left[find_char_pos(left, '+')+1:],op,right)
+            op_side = "left"
+            left_left = left[0:find_char_pos(left, '+')]
+            left_right = left[find_char_pos(left, '+')+1:]
+            arithm_op = "+"
+            # return ("left",left[0:find_char_pos(left, '+')],"+",left[find_char_pos(left, '+')+1:],op,right)
         if find_char_pos(left, '-') != -1 :
-            return ("left",left[0:find_char_pos(left, '-')],"-",left[find_char_pos(left, '-')+1:],op,right)
+            op_side = "left"
+            left_left = left[0:find_char_pos(left, '+')]
+            left_right = left[find_char_pos(left, '+')+1:]
+            arithm_op = "-"
+            # return ("left",left[0:find_char_pos(left, '-')],"-",left[find_char_pos(left, '-')+1:],op,right)
         if find_char_pos(left, '*') != -1 :
-            return ("left",left[0:find_char_pos(left, '*')],"*",left[find_char_pos(left, '*')+1:],op,right)
+            op_side = "left"
+            left_left = left[0:find_char_pos(left, '+')]
+            left_right = left[find_char_pos(left, '+')+1:]
+            arithm_op = "*"
+            # return ("left",left[0:find_char_pos(left, '*')],"*",left[find_char_pos(left, '*')+1:],op,right)
         if find_char_pos(left, '/') != -1 :
-            return ("left",left[0:find_char_pos(left, '/')],"/",left[find_char_pos(left, '/')+1:],op,right)
-    for char in right:
-        if find_char_pos(right, '+') != -1 :
-            return ("right",left,op,right[0:find_char_pos(right, '+')],"+",right[find_char_pos(right, '+')+1:])
-        if find_char_pos(right, '-') != -1 :
-            return ("right",left,op,right[0:find_char_pos(right, '-')],"-",right[find_char_pos(right, '-')+1:])
-        if find_char_pos(right, '*') != -1 :
-            return ("right",left,op,right[0:find_char_pos(right, '*')],"*",right[find_char_pos(right, '*')+1:])
-        if find_char_pos(right, '/') != -1 :
-            return ("right",left,op,right[0:find_char_pos(right, '/')],"/",right[find_char_pos(right, '/')+1:])
-    return ("no_op",left,op,right)
+            op_side = "left"
+            left_left = left[0:find_char_pos(left, '+')]
+            left_right = left[find_char_pos(left, '+')+1:]
+            arithm_op = "/"
+            # return ("left",left[0:find_char_pos(left, '/')],"/",left[find_char_pos(left, '/')+1:],op,right)
+    # for char in right:
+    #     if find_char_pos(right, '+') != -1 :
+    #         op_side = "right"
+    #         right_left = right[0:find_char_pos(right, '+')]
+    #         right_right = right[find_char_pos(right, '+')+1:]
+    #         arithm_op = "+"
+    #         return ("right",left,op,right[0:find_char_pos(right, '+')],"+",right[find_char_pos(right, '+')+1:])
+    #     if find_char_pos(right, '-') != -1 :
+    #         return ("right",left,op,right[0:find_char_pos(right, '-')],"-",right[find_char_pos(right, '-')+1:])
+    #     if find_char_pos(right, '*') != -1 :
+    #         return ("right",left,op,right[0:find_char_pos(right, '*')],"*",right[find_char_pos(right, '*')+1:])
+    #     if find_char_pos(right, '/') != -1 :
+    #         return ("right",left,op,right[0:find_char_pos(right, '/')],"/",right[find_char_pos(right, '/')+1:])
+    return (left_left, arithm_op, left_right, binary_op, right)
+
 
 def check_num_table(left, right):
-    
-    pass
+    table_l = find_char_pos(left, '.')
+    table_r = find_char_pos(right, '.')
+    if table_l != -1 and table_r != -1:
+        left_t = left[0:table_l]
+        right_t = right[0:table_r]
+        left_col = left[table_l + 1:]
+        right_col = right[table_r + 1:]
+        if left_t == right_t:
+            return (1, left_t, None, left_col, None)
+        else:
+            return (2, left_t, right_t, left_col, right_col)
+    else:
+        return (1, left_t, None, left_col, None)
 
-def filter_comp():
-    pass
-
-def merge_comp():
-    pass
 
 def comparision_parse(item):
     if (find_char_pos(item, '<') != -1 and find_char_pos(item, '=') != -1):
-            side, left, op, right = (item[0:find_char_pos(item, '<')]),"<=",(item[find_char_pos(item, '=')+1:])
-            ntable = check_num_table(left, right)
+            left, op, right = (item[0:find_char_pos(item, '<')]),"<=",(item[find_char_pos(item, '=')+1:])
     elif (find_char_pos(item, '>') != -1 and find_char_pos(item, '=') != -1):
-            side, left, op, right =  (item[0:find_char_pos(item, '>')]),">=",(item[find_char_pos(item, '=')+1:])
-            ntable = check_num_table(left, right)
+            left, op, right =  (item[0:find_char_pos(item, '>')]),">=",(item[find_char_pos(item, '=')+1:])
     elif (find_char_pos(item, '<') != -1 and find_char_pos(item, '>') != -1):
-            side, left, op, right =  (item[0:find_char_pos(item, '<')]),"<>",(item[find_char_pos(item, '>')+1:])
-            ntable = check_num_table(left, right)
+            left, op, right =  (item[0:find_char_pos(item, '<')]),"<>",(item[find_char_pos(item, '>')+1:])
     elif find_char_pos(item, '<') != -1:
-            side, left, op, right =  (item[0:find_char_pos(item, '<')]),"<",(item[find_char_pos(item, '<')+1:])
-            ntable = check_num_table(left, right)
+            left, op, right =  (item[0:find_char_pos(item, '<')]),"<",(item[find_char_pos(item, '<')+1:])
     elif find_char_pos(item, '>') != -1:
             side, left, op, right =  (item[0:find_char_pos(item, '>')]),">",(item[find_char_pos(item, '>')+1:])
-            ntable = check_num_table(left, right)
     elif find_char_pos(item, '=') != -1:
-            side, left, op, right =  (item[0:find_char_pos(item, '=')]),"=",(item[find_char_pos(item, '=')+1:])
-            ntable = check_num_table(left, right)
+            left, op, right =  (item[0:find_char_pos(item, '=')]),"=",(item[find_char_pos(item, '=')+1:])
 
-    if ntable == 1:
-        filter_comp()
-    else:
-        merge_comp()
+    return (left, op, right)
 
 def find_char_pos(string, char):
     if char in string:
